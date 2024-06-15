a7b6ae10712b0fd35bab174e15f232d9159112d4
rabby-shek <rabbyshek1998@gmail.com>
2024-06-16 00:40:35 +0600

    website published to github

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import EmailImg from "../../assets/images/footer-email.png";
 const Footer = () => {
-    const date = new Date();
-    const year = date.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <section id="contact">
-        <footer className="footer section-padding">
-      <div className="footer-top">
-        <div className="footer-top-text drop-shadow">
-          <img src={EmailImg} alt="email" />
-          <p>
-            Sign Up to Get Updates & <br /> News About Us.
-          </p>
+      <footer className="footer section-padding">
+        <div className="footer-top">
+          <div className="footer-top-text drop-shadow">
+            <img src={EmailImg} alt="email" />
+            <p>
+              Sign Up to Get Updates & <br /> News About Us.
+            </p>
+          </div>
+          <div className="footer-input-container">
+            <input type="text" name="" id="" placeholder="Email Address" />
+            <button className="drop-shadow">Subscribe</button>
+          </div>
         </div>
-        <div className="footer-input-container">
-          <input type="text" name="" id="" placeholder="Email Address" />
-          <button className="drop-shadow">Subscribe</button>
+        <hr className="divider" />
+        <div className="footer-bottom"></div>
+        <div className="copy-right">
+          Copyright © {year} MD Rabby Shek Suvo. All Rights Reserved.
         </div>
-      </div>
-      <hr className="divider" />
-      <div className="footer-bottom">
-
-      </div>
-      <div className="copy-right">Copyright © {year} MD Rabby Shek Suvo. All Rights Reserved.</div>
-    </footer>
+      </footer>
     </section>
   );
 };
